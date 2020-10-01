aeda96fd2ceb8615aed392264b6428dde79a9b2f
sebas951102 <71895352+sebas951102@users.noreply.github.com>
2020-09-30 19:07:00 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,33 @@
 
 
-## this function works to cache a matrix
+## this function works to cache a matrix called X
 
 makeCacheMatrix <- function(x = matrix()) {
   inversa <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y ##cache
     inversa <<- NULL
   }
   get <- function() x
-  setinversa <- function(inverse) inversa <<- inverse
+  setinversa <- function(inverse) inversa <<- inverse 
   getinversa <- function() inversa
   list(set = set, get = get, setinversa = setinversa, getinversa = getinversa)
 }
 
-## cachesolve returns the inverse of X, it search on the cached data, if i
+## cachesolve returns the inverse of X, it search on the cached data, if the inverse matrix is cached
+## it just return that, if not, it calculates and return the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   ## Return an inverse matrix 
-  inversa <- x$getinversa()
-  if(!is.null(inversa)) {
+  
+  inversa <- x$getinversa() #search the inverse
+  if(!is.null(inversa)) {  #so the matrix is chached
     message("getting cached result")
-    return(inversa)
+    return(inversa)       #and it ends
   }
   datos <- x$get()
-  inversa <- solve(datos, ...)
+  inversa <- solve(datos, ...) #if its not cached
   x$setinversa(inversa)
   inversa
   
